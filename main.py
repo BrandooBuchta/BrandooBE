1b5b91cf8a97380424bf820bcb169f286012da8b
BrandooBuchta <buchta@brandoo.cz>
2024-08-07 20:26:05 +0200

    just run

--- main.py
+++ main.py
@@ -83,4 +83,5 @@ def shutdown_event():
     scheduler.shutdown()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app)
