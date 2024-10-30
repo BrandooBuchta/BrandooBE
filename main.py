e6e323f174fb75a10d82cd94421fee955f5a1bb7
BrandooBuchta <buchta@brandoo.cz>
2024-10-30 23:10:11 +0100

    force redeploy

--- main.py
+++ main.py
@@ -70,10 +70,10 @@ public_endpoints_regex = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods (GET, POST, PUT, DELETE, etc.)
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.middleware("http")
